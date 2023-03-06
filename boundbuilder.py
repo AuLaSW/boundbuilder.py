98a1a933f5d2a18fc64ffc43dce581a2e54c82af
Austin <austin.swanlaw@gmail.com>
2023-03-06 13:12:14 -0600

    Cleanup main file
    Cleanup the main file to remove functions that are not being used. This
    just makes the file a bit smaller.

--- boundbuilder.py
+++ boundbuilder.py
@@ -14,59 +14,14 @@ import pdb
 import sys
 
 
-def mergePdfs(paths, output):
-    """
-    Params:
-    -------
-        paths: an iterable collection of paths of pdfs to merge.
-        
-        output: the Path object for the output file.
-    
-    Returns:
-    --------
-        None
-    
-    Action:
-    -------
-        Takes the pdfs found in paths and merges them into one pdf
-        file and saves the pdf file to output.
-    """
-    pdf_writer = PdfWriter()
-    
-    for path in paths[output]:
-        try:
-            pdf_reader = PdfReader(path)
-        except FileNotFoundError:
-            boundname = depathize(path)
-            
-            if path in paths:
-                mergePdfs(
-                    output=path,
-                    paths=paths
-                )
-                
-                pdf_reader = PdfReader(path)
-                #output.remove(boundname)
-            else:
-                #tb = sys.exception().__traceback__
-                raise FileNotFoundError(f"File {boundname}.pdf could not be found.")
-        
-        print(path)
-        for page in range(len(pdf_reader.pages)):
-            pdf_writer.add_page(pdf_reader.pages[page])
-    
-    with open(output, 'wb') as out:
-        pdf_writer.write(out)
-
-
-def mergePdfsPike(paths, output, pf):
+def mergePdfs(paths, output, pf):
     pdf_writer = pikepdf.Pdf.new()
     
     for file in paths[output]:
         print(file)
         
         if file in paths:
-            mergePdfsPike(
+            mergePdfs(
                 paths=paths,
                 output=file,
                 pf=pf
@@ -122,64 +77,8 @@ def configParser(cfg, CWD, sets):
             raise RuntimeError(f"The line \"{line.strip()}\" in .config is not correctly formatted.")
 
 
-def configParser2(cfg, CWD):
-    # dictionary of bound sets
-    sets = {}
-    
-    curline = cfg.readline()
-    nextline = cfg.readline()
-    
-    curlinelvl = level(curline)
-    nextlinelvl = level(nextline)
-    
-    if curlinelvl < nextlinelvl:
-        """
-        curline
-            nextline
-        """
-        prevcbs = cbs
-        cbs = pathize(curline, CWD)
-        sets[cbs] = []
-        # if we are already inside of a bound set,
-        # append the name of the current file to
-        # the list of files to bind to the original
-        # set.
-        if cbs >= 2:
-            sets[prevcbs].append(cbs)
-    elif curlinelvl == nextlinelvl:
-        """
-        curline
-        nextline
-        """
-        sets[cbs].append(pathize(curline, CWD))
-    elif curlinelvl > nextlinelvl:
-        """
-            curline
-        nextline
-        """
-        sets[cbs].append(pathize(curline, CWD))
-        cbs = prevcbs
-    
-    return sets
-
-
 def pathize(line, CWD):
     return CWD / (line.strip() + '.pdf')
-
-
-def depathize(path):
-    return path.parts[-1].replace('.pdf', '')
-
-
-def level(line: str) -> int:
-    accum = Increment()
-    for char in line:
-        if char == '\t':
-            +accum
-        else:
-            break
-    
-    return accum.value
 
 
 def main():
@@ -199,43 +98,66 @@ def main():
     for output in files.keys():
         if output in processedfiles:
             try:
-                mergePdfsPike(
+                mergePdfs(
                     output=output,
                     paths=files,
                     pf=processedfiles
                 )
             except FileNotFoundError as err:
                 messagebox.showerror("Error!", err)
-    
-    """
-    for boundset, mergers in files.items():
-        mergePdfs(output=boundset, paths=mergers)
-    """
-    
-    """
-    pdf_writer = PdfWriter()
-    
-    for path in path[output]:
-        try:
-            pdf_reader = PdfReader(path)
-        except FileNotFoundError:
-            boundname = depathize(path)
-            
-            if boundame in output:
-                mergePdfs(
-                    output=path,
-                    paths=paths
-                )
-                output.remove(boundname)
+
+
+class SMPLParser:
+    def configParser2(cfg, CWD):
+        # dictionary of bound sets
+        sets = {}
+        
+        curline = cfg.readline()
+        nextline = cfg.readline()
+        
+        curlinelvl = level(curline)
+        nextlinelvl = level(nextline)
+        
+        if curlinelvl < nextlinelvl:
+            """
+            curline
+                nextline
+            """
+            prevcbs = cbs
+            cbs = pathize(curline, CWD)
+            sets[cbs] = []
+            # if we are already inside of a bound set,
+            # append the name of the current file to
+            # the list of files to bind to the original
+            # set.
+            if cbs >= 2:
+                sets[prevcbs].append(cbs)
+        elif curlinelvl == nextlinelvl:
+            """
+            curline
+            nextline
+            """
+            sets[cbs].append(pathize(curline, CWD))
+        elif curlinelvl > nextlinelvl:
+            """
+                curline
+            nextline
+            """
+            sets[cbs].append(pathize(curline, CWD))
+            cbs = prevcbs
+        
+        return sets
+
+
+    def level(line: str) -> int:
+        accum = Increment()
+        for char in line:
+            if char == '\t':
+                +accum
             else:
-                raise FileNotFoundError
-            
-        for page in range(len(pdf_reader.pages)):
-            pdf_writer.add_page(pdf_reader.pages[page])
-    
-    with open(output, 'wb') as out:
-        pdf_writer.write(out)
-    """
+                break
+        
+        return accum.value
 
 
 if __name__ == "__main__":
